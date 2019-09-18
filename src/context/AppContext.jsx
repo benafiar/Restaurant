1c484fb900cc9d4e4ae81d4e4c1b9784b43813ee
Lazaro <lquin008@gmail.com>
2019-09-18 11:47:37 -0400

    context for sections

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,25 +1,31 @@
-import React, { createContext, useState, useEffect, useCallback } from "react";
+import React, { createContext, useState, useEffect, useCallback } from 'react';
 
-const axios = require("axios");
+const axios = require('axios');
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
-const [sections, setSections] = useState([])
+  const [sections, setSections] = useState([]);
 
-  const fetchSections = async () => {
-    let  { data }  = await axios.get(`api/sections`);
-    console.log(data)
-    setSections([...data]);
+  const setMenuSections = async () => {
+    const { data } = await axios.get(`/api/menu`);
+    const refs = data.options.map(section => section._ref);
+    const items = await axios.get(`/api/sections`);
+    const categories = items.data.filter(item => {
+      return refs.includes(item._id);
+    });
+    setSections([...categories]);
   };
 
-  const mountEffect = useCallback(fetchSections, []);
+  const fetchData = useCallback(async () => {
+    setMenuSections();
+  }, []);
 
   useEffect(() => {
-    mountEffect()
-  }, [mountEffect]);
+    fetchData();
+  }, [fetchData]);
 
   return (
-    <AppContext.Provider value={{ sections, setSections }}>
+    <AppContext.Provider value={{ sections }}>
       {children}
     </AppContext.Provider>
   );
