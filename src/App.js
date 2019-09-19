4a3efb8922651d2941e7d047dc2102ba26823b5b
Lazaro <lquin008@gmail.com>
2019-09-18 20:52:20 -0400

    first route added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import  Layout  from "./containers/Layout/Layout";
 import { AppContextProvider } from "./context/AppContext";
 import Home from './pages/Home/Home'
+import Beef from './pages/Beef/Beef'
 
 const App = () => (
   <BrowserRouter>
     <AppContextProvider>
         <Layout>
           <Route exact path="/" component={Home} />
+          <Route exact path="/flame_grilled_beef" component={Beef} />
         </Layout>
     </AppContextProvider>
   </BrowserRouter>
---
+++ src/pages/Beef/Beef.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Beef = () => <h1>BEEF</h1>;
+
+export default Beef;
