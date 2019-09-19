a53c0707f8840d60277424a3f1cab955cc8d0535
Lazaro <lquin008@gmail.com>
2019-09-18 22:59:15 -0400

    routes minus home page working

--- src/pages/ComboMeals/ComboMeals.jsx
+++ src/pages/ComboMeals/ComboMeals.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { MenuGrid } from '../../components'
+import { AppContext } from '../../context/AppContext';
+import gridHelper from '../../helpers/gridHelper';
 
-const ComboMeals = () => {
+const ComboMeals = (props) => {
+  const { allItems, setMenuGridItems, sections } = useContext(AppContext);
+
+  const gridContents = sections.map(section => {
+    return {
+      gridItems: section.options.map(option => option._ref)
+    };
+  });
+
+  useEffect(()=>{
+    const currentSelection = [...allItems.flat()].filter(item => {
+      return gridContents[gridHelper(props.match.path)].gridItems.includes(item._id);
+    });
+    setMenuGridItems([...currentSelection]);
+  }, [])
+
   return(
     <div>
       <h1>ComboMeals</h1>
