92a435f6fec7bac34a4e4abe7c535ce2018647d4
Lazaro <lquin008@gmail.com>
2019-09-22 18:58:28 -0400

    content page created

---
+++ src/pages/Content/Content.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { withRouter } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import { PageView, PlaceHolder } from '../../components';
+import { routeMapper } from '../../helpers';
+
+const Content = ({ location, match }) => {
+  const { loaded } = useContext(AppContext);
+  const routeProps = routeMapper(location.pathname);
+
+  return (
+    <div>
+      {loaded ? (
+        <PageView match={match.path} {...routeProps} />
+      ) : (
+        <PlaceHolder />
+      )}
+      <div></div>
+    </div>
+  );
+};
+
+export default withRouter(Content);
