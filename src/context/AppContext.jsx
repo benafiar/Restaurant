521007617323ee4eba2d0c7832ca134f67c45056
Lazaro <lquin008@gmail.com>
2019-09-22 18:56:57 -0400

    updated context

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,48 +1,54 @@
 import React, { createContext, useState, useEffect, useCallback } from 'react';
-import gridHelper from '../helpers/gridHelper';
+import { withRouter } from 'react-router-dom';
+import { gridHelper } from '../helpers';
+import axios from 'axios';
 
-const axios = require('axios');
 const AppContext = createContext();
 
-const AppContextProvider = ({ children }) => {
+const AppContextProvider = withRouter(({ location, children }) => {
   const [sections, setSections] = useState([]);
   const [menuGridItems, setMenuGridItems] = useState([]);
   const [allItems, setAllItems] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
-  const setMenuSections = async () => {
-    const { data } = await axios.get(`/api/menu`);
-    const refs = data.options.map(section => section._ref);
-    const items = await axios.get(`/api/sections`);
-    const categories = items.data.filter(item => {
-      return refs.includes(item._id);
-    });
-    const fullMenu = await axios.get('api/items');
-    setSections([...categories]);
-    setAllItems([fullMenu.data]);
-    if (window.location.pathname === '/') {
-      setMenuGridItems([...categories]);
-    } else {
-      const gridContents = [...categories].map(section => {
-        return {
-          gridItems: section.options.map(option => option._ref)
-        };
-      });
-      const currentSelection = [...fullMenu.data.flat()].filter(item => {
-        return gridContents[
-          gridHelper(window.location.pathname)
-        ].gridItems.includes(item._id);
-      });
-      setMenuGridItems([...currentSelection]);
-    }
-  };
-
-  const fetchData = useCallback(() => {
-    setMenuSections();
+  const fetchData = useCallback(async () => {
+    const requests = [
+      axios.get('/api/menu'),
+      axios.get('/api/sections'),
+      axios.get('/api/items')
+    ];
+    const [
+      { data: menuData = {} },
+      { data: sectionData = [] },
+      { data: fullMenuData = [] }
+    ] = await Promise.all(requests);
+    const refs = menuData.options.map(section => section._ref);
+    const categories = sectionData.filter(item => refs.includes(item._id));
+    setSections(categories);
+    setAllItems(fullMenuData);
+    setLoaded(true);
   }, []);
+
+  const setMenuSections = useCallback(() => {
+    if (location.pathname === '/') return setMenuGridItems(sections);
+    const gridContents = sections.map(section => ({
+      gridItems: section.options.map(option => option._ref)
+    }));
+    const currentSelection = allItems.filter(item =>
+      gridContents[gridHelper(window.location.pathname)].gridItems.includes(
+        item._id
+      )
+    );
+    setMenuGridItems([...currentSelection]);
+  }, [allItems, location.pathname, sections]);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
+
+  useEffect(() => {
+    loaded && setMenuSections();
+  }, [loaded, location.pathname, setMenuSections]);
 
   return (
     <AppContext.Provider
@@ -51,12 +57,12 @@ const AppContextProvider = ({ children }) => {
         menuGridItems,
         setMenuGridItems,
         allItems,
-        setAllItems
+        loaded
       }}
     >
       {children}
     </AppContext.Provider>
   );
-};
+});
 
 export { AppContext, AppContextProvider };
