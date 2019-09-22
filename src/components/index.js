80252526be42156830f20882cc6e6fb1b35bf29b
Lazaro <lquin008@gmail.com>
2019-09-22 19:03:35 -0400

    components updated

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as MenuGrid } from "./MenuGrid/MenuGrid";
 export { default as GridItem } from "./GridItem/GridItem";
 export { default as NavItem } from "./NavItem/NavItem";
 export { default as PageView } from "./PageView/PageView";
+export { default as PlaceHolder } from "./PlaceHolder/PlaceHolder";
